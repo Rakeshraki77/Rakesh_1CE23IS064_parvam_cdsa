6d25c158c1af28973bc17be881bba04384093fb5
Rakeshraki77 <rakiirakesh317@gmail.com>
2024-11-10 16:01:34 +0530

    struct programs and basic

---
+++ c pro/datatypes.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main() {
+    int id=100;
+    char status='y';
+    float fees=200000.00f;
+    double totalfees=fees*4;
+    printf("%d\n", id);
+    printf("%c\n",status);
+    printf("%f\n",fees);
+    printf("%lf\n",totalfees);
+    return 1;
+}
---
+++ c pro/first.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+void main(){
+    int a=100;
+ printf("welcome to c program...");
+ puts("enjoy c program learning...");
+ printf("%d",a*100);
+}
+return
